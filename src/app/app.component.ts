1c552b1a0f860677da04c61c1bd88f50953d6214
amit-negi-23 <amitnegiit23@gmail.com>
2024-02-22 14:32:04 +0530

    Property Binding (see the comment inside file)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,11 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    @for(user of users; track user.id){
-      <h3> {{user.name}}</h3>
-    }
+    <button [disabled]="status"> Click me!</button>
   `,
   styles: `h3{color: red}`
 
 })
 export class AppComponent {
-  users=[{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
+  status = true;
 }
